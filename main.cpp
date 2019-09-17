b79f6bef9213d02b9a8739c96c97d2f9d52a7061
李寻欢 <zqt3926234@gmail.com>
2019-09-17 19:46:07 +0800

    PAT

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 郑期腾 on 2019/9/17.
+//  Copyright © 2019 李寻欢. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
