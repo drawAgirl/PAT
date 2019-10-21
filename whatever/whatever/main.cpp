168b98a07f30af511a677290cc71c53c66b667a8
李寻欢 <zqt3926234@gmail.com>
2019-10-21 17:17:03 +0800

    update

--- whatever/whatever/main.cpp
+++ whatever/whatever/main.cpp
@@ -20,28 +20,69 @@
 #include <set>
 #include <deque>
 using namespace std;
-const int MAXN = 1e5+10;
-int n,m;
-int diamonds[MAXN]={0};
+
+const int MAXN = 1e4+10;
+int n;
+unordered_map<int, vector<int>> g;
+bool vis[MAXN] = {false};
+int max_depth = 0;
+vector<int> deep_root;
+
+void dfs(int root,int &cnt){
+    if(vis[root]) return;
+    vis[root] = true;
+    cnt++;
+    for(auto &each:g[root]) dfs(each,cnt);
+}
 
 int main(){
-    scanf("%d%d",&n,&m);
-    for(int i=1;i<=n;++i){
-        scanf("%d",&diamonds[i]);
-        diamonds[i] += diamonds[i-1];
+    scanf("%d",&n);
+    if(n == 1){
+        printf("1");
+        return 0;
     }
-    int ans = MAXN;
+    int a,b;
+    for(int i=1;i<n;++i){
+        scanf("%d%d",&a,&b);
+        g[a].emplace_back(b);
+        g[b].emplace_back(a);
+    }
+    int components = 0;
+    int cnt = 0;
+    bool flag = false;
     for(int i=1;i<=n;++i){
-        int j = upper_bound(diamonds+i, diamonds+n+1, diamonds[i-1]+m) - begin(diamonds);
-        if(diamonds[j-1] - diamonds[i-1] == m){
-            ans = m;
-            break;
+        fill(vis, vis+MAXN, false);
+        queue<int> q;
+        q.push(i);
+        int depth = 0;
+        while (!q.empty()) {
+            depth++;
+            int size = (int)q.size();
+            while (size--) {
+                int temp = q.front();
+                q.pop();
+                if(vis[temp] == false) cnt++;
+                vis[temp] = true;
+                for(auto &each:g[temp]){
+                    if(vis[each] == false) q.push(each);
+                }
+            }
         }
-        if(j<=n && diamonds[j]-diamonds[i-1] < ans) ans = diamonds[j]-diamonds[i-1];
+        if(depth > max_depth){
+            max_depth = depth;
+            deep_root.clear();
+            deep_root.emplace_back(i);
+        }else if (depth == max_depth) deep_root.emplace_back(i);
+        if(flag) continue;
+        if(cnt == n){
+            flag = true;
+            continue;
+        }else components++;
+        
     }
-    for(int i=1;i<=n;++i){
-        int j = upper_bound(diamonds+i, diamonds+n+1, diamonds[i-1]+ans) - begin(diamonds);
-        if(diamonds[j-1] - diamonds[i-1] == ans)
-            printf("%d-%d\n",i,j-1);
+    if(components > 0) printf("Error: %d components\n",components+1);
+    else{
+        for(int i=0;i<(int)deep_root.size();++i) printf("%d\n",deep_root[i]);
+        
     }
 }
